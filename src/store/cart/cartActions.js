6e348b869584fb5a39da72503f3164a930489da9
DevWaleedS <eng.waleed.salah@outlook.com>
2023-06-11 19:14:58 +0300

    last update

--- src/store/cart/cartActions.js
+++ src/store/cart/cartActions.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { toast } from 'react-toastify';
-import { CART_SET_ITEM, CART_ADD_ITEM, CART_REMOVE_ITEM, CART_UPDATE_QUANTITIES } from './cartActionTypes';
+import axios from "axios";
+import { toast } from "react-toastify";
+import { CART_SET_ITEM, CART_ADD_ITEM, CART_REMOVE_ITEM, CART_UPDATE_QUANTITIES } from "./cartActionTypes";
 
 export function cartUpdateQuantitiesSuccess(quantities) {
     return {
@@ -10,90 +10,89 @@ export function cartUpdateQuantitiesSuccess(quantities) {
 }
 
 export function cartAddItem(product, options = [], quantity = 1) {
-    const token = localStorage.getItem('token');
-    const productData ={
-        id:product?.id,
-        name:product?.name,
-        price:product?.selling_price,
-        qty:quantity
-    }
+    const token = localStorage.getItem("token");
+    const productData = {
+        id: product?.id,
+        name: product?.name,
+        price: product?.selling_price,
+        qty: quantity,
+    };
     let resultData = null;
     return async function (dispatch) {
         try {
-            const response = await axios.post(`https://backend.atlbha.com/api/addCart`,productData, {
+            const response = await axios.post(`https://backend.atlbha.com/api/addCart`, productData, {
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: `Bearer ${token}`,
                 },
             });
-            if(response?.data?.success === true){
-                toast.success(`منتج "${productData?.name}" تمت اضافته للسلة !`, { theme: 'colored' });
+            if (response?.data?.success === true) {
+                toast.success(`تم إضافة  "${productData?.name}"للسلة `, { theme: "colored" });
                 resultData = response?.data?.data;
             }
         } catch (err) {
-            toast.error(err, { theme: 'colored' });
+            toast.error(err, { theme: "colored" });
         }
         dispatch({
             type: CART_ADD_ITEM,
-            data:resultData
+            data: resultData,
         });
-    }
+    };
 }
 
 export function cartRemoveItem(itemId) {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     let resultData = null;
     return async function (dispatch) {
         try {
             const response = await axios.get(`https://backend.atlbha.com/api/deleteCart/${itemId}`, {
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: `Bearer ${token}`,
                 },
             });
-            if(response?.data?.success === true){
+            if (response?.data?.success === true) {
                 resultData = response?.data?.data;
             }
         } catch (err) {
-            toast.error(err, { theme: 'colored' });
+            toast.error(err, { theme: "colored" });
         }
         dispatch({
             type: CART_REMOVE_ITEM,
-            data:resultData
+            data: resultData,
         });
-    }
+    };
 }
 
 export function cartUpdateQuantities(quantities) {
     // sending request to server, timeout is used as a stub
-    return (dispatch) => (
+    return (dispatch) =>
         new Promise((resolve) => {
             setTimeout(() => {
                 dispatch(cartUpdateQuantitiesSuccess(quantities));
                 resolve();
             }, 500);
-        })
-    );
+        });
 }
 
 export function fetchCartData() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     let resultData = null;
     return async function (dispatch) {
         try {
             const response = await axios.get("https://backend.atlbha.com/api/cartShow/1", {
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: `Bearer ${token}`,
                 },
             });
             resultData = response?.data?.data?.cart;
         } catch (err) {
-            toast.error(err, { theme: 'colored' });
+            toast.error(err, { theme: "colored" });
         }
         dispatch({
             type: CART_SET_ITEM,
-            data:resultData,
+            data: resultData,
         });
-    }
+    };
 }
